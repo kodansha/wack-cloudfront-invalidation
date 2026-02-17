752c73d794618f4a70a4e0fad79a6fb80b4e60f2
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2026-02-17 05:29:40 +0000

    fix phpunit settings

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-// Define PHPUNIT constant for test environment
-define('PHPUNIT', true);
-
 // First we need to load the composer autoloader, so we can use WP Mock
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
